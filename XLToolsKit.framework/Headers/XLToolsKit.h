b647733114e1d2a84f30e6c2c0ffb417a5397f15
xl <xl@shiendeMac-mini.lan>
2018-06-20 17:27:13 +0800

    添加动态库

---
+++ XLToolsKit.framework/Headers/XLToolsKit.h
@@ -0,0 +1,20 @@
+//
+//  XLToolsKit.h
+//  XLToolsKit
+//
+//  Created by xl on 2018/6/20.
+//  Copyright © 2018年 七叶昔洛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "XLTools.h"
+
+//! Project version number for XLToolsKit.
+FOUNDATION_EXPORT double XLToolsKitVersionNumber;
+
+//! Project version string for XLToolsKit.
+FOUNDATION_EXPORT const unsigned char XLToolsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XLToolsKit/PublicHeader.h>
+
+
